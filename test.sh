221cbd32aa266ba0522209a61cbde2641f793bfb
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-03-18 17:03:32 +0800

    show execution time

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 for input in ./test/input-*.txt; do
   expected=$(head -n 1 "$input" | cut -d' ' -f 2 | tr -d '\r\n')
-  actual=$(sed '1d' "$input"| python3 main.py | tr -d '\r\n')
-  if [[ $expected -eq $actual ]]; then
-    echo PASS
+  output=$(sed '1d' "$input"| time -p -f "%e" python3 main.py 2>&1)
+  actual=$(echo "$output" | head -n 1)
+
+  if [[ $expected -eq  $actual ]]; then
+    echo PASS "$(echo "$output" | tail -n 1)"s
   else
     echo FAILED "$input" expected "$expected" actual "$actual"
   fi
