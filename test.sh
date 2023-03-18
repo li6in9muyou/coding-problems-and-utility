9f7e48e25cac60c43528a9a40ee61c9d58244862
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-03-18 16:33:49 +0800

    apply shellcheck suggestions

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-for input in $(ls ./input-*.txt); do
+for input in ./test/input-*.txt; do
   expected=$(head -n 1 "$input" | cut -d' ' -f 2 | tr -d '\r\n')
-  actual=$(cat "$input" | sed '1d' | python3 main.py | tr -d '\r\n')
+  actual=$(sed '1d' "$input"| python3 main.py | tr -d '\r\n')
   if [[ $expected -eq $actual ]]; then
     echo PASS
   else
