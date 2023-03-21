9544c27ff3622a3125c0b11a289530f54d244e07
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-03-21 18:24:40 +0800

    add colors

--- test.sh
+++ test.sh
@@ -1,3 +1,15 @@
+green() {
+  tput setaf 2
+  cat
+  tput sgr0
+}
+
+red() {
+  tput setaf 1
+  cat
+  tput sgr0
+}
+
 for input in ./test/*.in; do
   output=$(/usr/bin/time -p -f "%e" timeout 1.5 python3 main.py 2>&1 < "$input")
   status="$?"
@@ -5,15 +17,15 @@ for input in ./test/*.in; do
   actual=$(echo "$output" | head -n 1)
   case "$status" in
     124)
-      echo TLE "$elapsed" "$input"
+      echo TLE "$elapsed" "$input" | red
       ;;
     *)
       if diff -u --color \
         --label actual <(echo "$actual") \
         --label expected "${input/in/out}"; then
-        echo PASS "$elapsed" "$input"
+        echo PASS "$elapsed" "$input" | green
       else
-        echo FAILED "$input"
+        echo FAILED "$input" | red
       fi
       ;;
   esac
