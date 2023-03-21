497e79fd49375f211187fe7fb4fbf82ddab7618f
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-03-21 18:22:46 +0800

    use diff to compare input and output

--- test.sh
+++ test.sh
@@ -1,18 +1,19 @@
-for input in ./test/input-*.txt; do
-  expected=$(head -n 1 "$input" | cut -d' ' -f 2 | tr -d '\r\n')
-  output=$(sed '1d' "$input"| time -p -f "%e" timeout 1.5 python3 main.py 2>&1)
+for input in ./test/*.in; do
+  output=$(/usr/bin/time -p -f "%e" timeout 1.5 python3 main.py 2>&1 < "$input")
   status="$?"
+  elapsed="$(echo "$output" | tail -n 1)s"
   actual=$(echo "$output" | head -n 1)
-
   case "$status" in
     124)
-      echo TLE "$input"
+      echo TLE "$elapsed" "$input"
       ;;
     *)
-      if [[ $expected -eq  $actual ]]; then
-        echo PASS "$(echo "$output" | tail -n 1)"s "$input"
+      if diff -u --color \
+        --label actual <(echo "$actual") \
+        --label expected "${input/in/out}"; then
+        echo PASS "$elapsed" "$input"
       else
-        echo FAILED "$input" expected "$expected" actual "$actual"
+        echo FAILED "$input"
       fi
       ;;
   esac
