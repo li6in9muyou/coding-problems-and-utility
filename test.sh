2e3210e8d5d7ae4bf3fe67a2784fa3f80da68e15
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-03-21 17:49:01 +0800

    test: add TLE error

--- test.sh
+++ test.sh
@@ -1,11 +1,19 @@
 for input in ./test/input-*.txt; do
   expected=$(head -n 1 "$input" | cut -d' ' -f 2 | tr -d '\r\n')
-  output=$(sed '1d' "$input"| time -p -f "%e" python3 main.py 2>&1)
+  output=$(sed '1d' "$input"| time -p -f "%e" timeout 1.5 python3 main.py 2>&1)
+  status="$?"
   actual=$(echo "$output" | head -n 1)
 
-  if [[ $expected -eq  $actual ]]; then
-    echo PASS "$(echo "$output" | tail -n 1)"s "$input"
-  else
-    echo FAILED "$input" expected "$expected" actual "$actual"
-  fi
+  case "$status" in
+    124)
+      echo TLE "$input"
+      ;;
+    *)
+      if [[ $expected -eq  $actual ]]; then
+        echo PASS "$(echo "$output" | tail -n 1)"s "$input"
+      else
+        echo FAILED "$input" expected "$expected" actual "$actual"
+      fi
+      ;;
+  esac
 done
